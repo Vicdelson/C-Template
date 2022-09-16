e53b759da0b6f6344acbfa7b7f78670e007c46f8
Vicdelson <daveyoehmi@gmail.com>
2022-09-16 16:30:29 +0800

    Power

--- try.c
+++ try.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+#include <math.h>
 
 int main(void)
 {
-    float value = 0;
-    for (int i=0; i<1; i++)value += 3;
-    printf("%f\n", value);
+    float x = 5.5, a = 4, b = 3, c = 2;
+    scanf("%f", &x);
+    float result = 7*pow(x, a)-8*pow(x,b)-pow(x,c)+6*x-22;
+    printf("%.1f", result);
     return 0;
 }
