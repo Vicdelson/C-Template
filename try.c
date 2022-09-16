6189f3913790002d748a7fcfbede2d4ec03db069
Vicdelson <daveyoehmi@gmail.com>
2022-09-16 16:31:26 +0800

    Power rule

--- try.c
+++ try.c
@@ -6,6 +6,6 @@ int main(void)
     float x = 5.5, a = 4, b = 3, c = 2;
     scanf("%f", &x);
     float result = 7*pow(x, a)-8*pow(x,b)-pow(x,c)+6*x-22;
-    printf("%.1f", result);
+    printf("%.1f\n", result);
     return 0;
 }
