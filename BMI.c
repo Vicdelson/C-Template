363427deeee94dcf6e2258e6b898433602e3d2ca
Vicdelson <daveyoehmi@gmail.com>
2022-09-16 23:15:40 +0800

    BMI

---
+++ BMI.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    float x = 167, y = 77;
+    scanf("%f %f", &x, &y);
+    x/=100;
+    x*=x;
+    float result = (y/x);
+    printf("%.6f", result);
+    return 0;
+}
